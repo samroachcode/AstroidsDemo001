b6769fe3d2251f097478ba39c6e9e7840ad667ff
DESKTOP-FL3NAD3\Sam <s.roach0305@gmail.com>
2018-08-26 13:48:32 +0100

    Basic Player controller

---
+++ Asteroids001/Assets/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InputManager : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody playerCharacter;
+    private Vector3 mousePos;
+
+    void Update()
+    {
+       
+    }
+
+    void FixedUpdate()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        playerCharacter.velocity = mousePos * movementSpeed;
+        playerCharacter.transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos);
+    }
+}
+
